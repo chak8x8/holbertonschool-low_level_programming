ec5163b70e0c530cf2b172916245e3ccdcc7fcdd
root <chak8x8@gmail.com>
2024-10-24 04:55:29 +0000

    7. Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
