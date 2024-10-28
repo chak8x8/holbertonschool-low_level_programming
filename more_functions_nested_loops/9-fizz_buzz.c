076e71d92e793f3dc1263f1704cb1a59874b1b04
root <chak8x8@gmail.com>
2024-10-28 08:33:46 +0000

    9. Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - prints the numbers from 1 to 100 with conditions
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3) == 0 && (i % 5) == 0)
+{
+printf("FizzBuzz");
+}
+else if ((i % 3) == 0)
+{
+printf("Fizz");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+
+if (i < 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+}
+
+int main(void)
+{
+fizz_buzz();
+return (0);
+}
