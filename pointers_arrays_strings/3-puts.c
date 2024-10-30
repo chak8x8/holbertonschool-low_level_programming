f0c9a76553656f88a251a06f8e931225ee1792f7
root <chak8x8@gmail.com>
2024-10-30 07:00:48 +0000

    _putchar.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout, followed by a new line.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+int i;
+for(i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
