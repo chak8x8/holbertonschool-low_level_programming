d703419347e65a0e7abf8cb5f82522118c3b7c3a
root <chak8x8@gmail.com>
2024-11-02 06:12:12 +0000

    6. Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, starting with the first.
- * @str: The string to be printed.
- */
+ * puts2 - prints part of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i += 2)
+int i = 0;
+while (str[i] != '\0')
 {
+if (i % 2 == 0)
 _putchar(str[i]);
+i++;
 }
+
 _putchar('\n');
 }
