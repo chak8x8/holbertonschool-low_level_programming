3f62defaaeeb1a6ba7ff7c9b64de1f7f5e67cab0
root <chak8x8@gmail.com>
2024-11-03 05:26:55 +0000

    2. strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string from src to dest.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy from src.
+ *
+ * Return: A pointer to the resulting dest string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+for (; i < n; i++)
+{
+if (src[i] != '\0')
+{
+dest[i] = src[i];
+}
+else
+{
+dest[i] = '\0';
+}
+}
+
+return (dest);
+}
