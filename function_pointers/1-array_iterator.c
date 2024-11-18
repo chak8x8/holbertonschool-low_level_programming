ae76595f0fc721e3327fa02911165dac03918290
root <chak8x8@gmail.com>
2024-11-18 08:39:22 +0000

    1. If you spend too much time thinking about a thing, you'll never get it done

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,7 +2,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function given as a parameter on each element of an array
+ * array_iterator - Executes a function given as a parameter
+ * on each element of an array
  * @array: The array to iterate over
  * @size: The size of the array
  * @action: A pointer to the function to be executed on each element
