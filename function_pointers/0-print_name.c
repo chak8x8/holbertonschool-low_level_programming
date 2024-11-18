b7ff66f9b2cab8153a7cb483b64bb3845abfdcf8
root <chak8x8@gmail.com>
2024-11-18 08:09:09 +0000

    0. What's my name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Calls a function to print a name
+ * @name: The name to print
+ * @f: Pointer to the function used for printing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if(f != NULL && name != NULL)
+{
+f(name);
+}
+}
