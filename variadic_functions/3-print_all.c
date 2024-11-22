4cef36eef70365396c48319e10ee4ff692dcfc58
root <chak8x8@gmail.com>
2024-11-22 05:30:41 +0000

    3. To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,58 +1,49 @@
-#include<stdio.h>
-#include<stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
- * struct printer - Struct for selecting correct printing function.
- * @symbol: Format symbol (e.g., "c", "i", "f", "s").
- * @print: Pointer to the corresponding print function.
- */
-typedef struct printer
-{
-char *symbol;
-void (*print)(va_list args);
-} printer_t;
-/**
- * print_char - Prints a character.
- * @args: Argument list containing the character to print.
+ * print_char - Prints a char.
+ * @args: List of arguments.
  */
 void print_char(va_list args)
 {
-char c = va_arg(args, int);
-printf("%c", c);
+printf("%c", va_arg(args, int));
 }
+
 /**
  * print_int - Prints an integer.
- * @args: Argument list containing the integer to print.
+ * @args: List of arguments.
  */
 void print_int(va_list args)
 {
-int n = va_arg(args, int);
-printf("%d", n);
+printf("%d", va_arg(args, int));
 }
+
 /**
  * print_float - Prints a float.
- * @args: Argument list containing the float to print.
+ * @args: List of arguments.
  */
 void print_float(va_list args)
 {
-double f = va_arg(args, double);
-printf("%f", f);
+printf("%f", va_arg(args, double));
 }
+
 /**
  * print_string - Prints a string.
- * @args: Argument list containing the string to print.
+ * @args: List of arguments.
  */
 void print_string(va_list args)
 {
-char *s = va_arg(args, char *);
-if (s == NULL)
+char *str = va_arg(args, char *);
+if (str == NULL)
 {
 printf("(nil)");
+return;
 }
-else
-{
-printf("%s", s);
+printf("%s", str);
 }
-}
+
 /**
  * print_all - Prints anything based on the format.
  * @format: Format string containing the types of arguments passed.
@@ -69,16 +60,19 @@ printer_t func[] = {
 {"s", print_string}
 };
 va_start(args, format);
-while (format != NULL && format[i] != '\0')
+while (format && format[i])
 {
 j = 0;
-while (j < 4 && *(format + i) != *(func[j].symbol))
-j++;
-if (j < 4)
+while (j < 4)
+{
+if (format[i] == *(func[j].symbol))
 {
 printf("%s", separator);
 func[j].print(args);
 separator = ", ";
+break;
+}
+j++;
 }
 i++;
 }
