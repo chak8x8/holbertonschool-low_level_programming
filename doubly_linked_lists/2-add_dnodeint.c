16b2750472dd8c69267077d78f7239aceae98644
root <chak8x8@gmail.com>
2024-12-04 09:18:22 +0000

    2. Add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,14 +12,14 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
-	
+
 	if (head == NULL)
 	{
 		return (NULL);
 	}
 
 	node = malloc(sizeof(dlistint_t));
-	if (node = NULL)
+	if (node == NULL)
 	{
 		free(node);
 		return (NULL);
