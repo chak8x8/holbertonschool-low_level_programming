6393d1c0854d97e231246769a462c437925f872e
root <chak8x8@gmail.com>
2024-12-05 08:13:14 +0000

    7. Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,8 +35,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (current == NULL || (current->next == NULL && i < idx - 1))
+	{
 		free(node);
 		return (NULL);
+	}
 
 	if (current->next == NULL)
 		return (add_dnodeint_end(h, n));
