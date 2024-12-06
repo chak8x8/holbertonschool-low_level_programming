0989824774580f79e50c9b50160b4e4965558570
root <chak8x8@gmail.com>
2024-12-06 10:26:38 +0000

    5. 101

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits - Counts the number of bits to flip to get from one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result = 0;
+	unsigned int count = 0;
+
+	result = n ^ m;
+
+	while (result !=  0)
+	{
+		if ((result & 1) == 1)
+		{
+			count++;
+		}
+		result = result >> 1;
+	}
+
+	return (count);
+}
