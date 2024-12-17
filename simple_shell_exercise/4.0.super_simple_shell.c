43f83a631bdd480c38f09f9f4980a24e3df0e4cf
root <chak8x8@gmail.com>
2024-12-17 12:53:48 +0000

    simple shell exercise

--- simple_shell_exercise/4.0.super_simple_shell.c
+++ simple_shell_exercise/4.0.super_simple_shell.c
@@ -1,3 +1,6 @@
+/* Exercise: super simple shell
+ * Using everything we saw, write a first version of a super simple shell that can run commands with their full path, without any argument.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
