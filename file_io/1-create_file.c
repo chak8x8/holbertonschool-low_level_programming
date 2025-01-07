549c78add1168bfa4a5d524291188371a687d5f2
root <chak8x8@gmail.com>
2025-01-07 06:57:08 +0000

    1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,12 +19,12 @@ int create_file(const char *filename, char *text_content)
 
 	/* Step 1: Validate input */
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	/* Step 2: Open the file */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	/* Step 3: Write content to the file (if any) */
 	if (text_content != NULL)
