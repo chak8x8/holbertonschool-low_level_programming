ecb516000ee557a5b5958c6d7473b5d0a8061b26
chak8x8 <chak8x8@gmail.com>
2025-08-11 17:17:25 +1000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,53 +1,95 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * main - function copies content of one file to another
- *
- *@argc: number of arguments
- *@argv: char * to input arguments
- *
- * Return: 0
- */
+#define BUF_SIZE 1024
+
+static int xopen_read(const char *name)
+{
+	int fd = open(name, O_RDONLY);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		exit(98);
+	}
+	return (fd);
+}
+
+static int xopen_write_trunc(const char *name)
+{
+	int fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+		exit(99);
+	}
+	return (fd);
+}
+
+static void xclose(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+static void copy_stream(int fd_from, const char *from_name, const char *to_name)
+{
+	char buf[BUF_SIZE];
+	ssize_t r, w, off;
+	int fd_to;
+
+	/* Probe read first so a read error exits 98 before touching destination */
+	r = read(fd_from, buf, BUF_SIZE);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name);
+		exit(98);
+	}
+
+	fd_to = xopen_write_trunc(to_name);
+
+	while (r > 0)
+	{
+		for (off = 0; off < r; off += w)
+		{
+			w = write(fd_to, buf + off, r - off);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_name);
+				xclose(fd_to);
+				exit(99);
+			}
+		}
+		r = read(fd_from, buf, BUF_SIZE);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name);
+			xclose(fd_to);
+			exit(98);
+		}
+	}
+	xclose(fd_to);
+}
 
 int main(int argc, char *argv[])
 {
-	int fd1, fd2;
-	ssize_t lread, lwrite;
-	char buff[1024];
+	int fd_from;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	if (argv[1] == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd2 = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
-	if (fd2 < 0)
-		fd2 = open(argv[2], O_TRUNC | O_WRONLY);
-	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while (lread)
-	{
-		lread = read(fd1, buff, 1024);
-		if (lread == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		lwrite = write(fd2, buff, lread);
-		if (lwrite == -1 || lwrite != lread)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-	if (close(fd1) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
-	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+
+	fd_from = xopen_read(argv[1]);
+	copy_stream(fd_from, argv[1], argv[2]);
+	xclose(fd_from);
 	return (0);
 }
