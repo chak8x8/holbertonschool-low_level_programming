41ad8d780753bd843bc18ed7cf9a5501e98494a7
chak8x8 <chak8x8@gmail.com>
2025-08-11 15:47:16 +1000

    varadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,15 +9,16 @@
  */
 typedef struct printer
 {
-char *symbol;
-void (*print)(va_list args);
+    char *symbol;
+    void (*print)(va_list args);
 } printer_t;
 
-void print_all(const char * const format, ...);
+int sum_them_all(const unsigned int n, ...); /* Add this line */
+
+void print_all(const char *const format, ...);
 void print_char(va_list args);
 void print_int(va_list args);
 void print_float(va_list args);
 void print_string(va_list args);
 
 #endif /* VARIADIC_FUNCTIONS_H */
-
