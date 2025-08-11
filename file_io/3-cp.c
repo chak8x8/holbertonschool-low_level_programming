aaffd0f8b8cff9535dfa8cc7f5770c82cd9ee94d
chak8x8 <chak8x8@gmail.com>
2025-08-11 17:19:52 +1000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,36 +1,55 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 #define BUF_SIZE 1024
 
-static int xopen_read(const char *name)
+/**
+ * open_file_for_read - Opens a file in read-only mode.
+ * @filename: The name of the file to open.
+ *
+ * Return: File descriptor on success, exits with 98 on failure.
+ */
+int open_file_for_read(const char *filename)
 {
-	int fd = open(name, O_RDONLY);
+	int fd;
 
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
 	return (fd);
 }
 
-static int xopen_write_trunc(const char *name)
+/**
+ * open_file_for_write - Opens a file for writing, creating or truncating it.
+ * @filename: The name of the file to open.
+ *
+ * Return: File descriptor on success, exits with 99 on failure.
+ */
+int open_file_for_write(const char *filename)
 {
-	int fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int fd;
 
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
 	return (fd);
 }
 
-static void xclose(int fd)
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to close.
+ *
+ * Return: None, exits with 100 on failure.
+ */
+void close_file(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -39,13 +58,20 @@ static void xclose(int fd)
 	}
 }
 
-static void copy_stream(int fd_from, const char *from_name, const char *to_name)
+/**
+ * copy_file_content - Copies content from one file descriptor to another.
+ * @fd_from: Source file descriptor.
+ * @from_name: Source file name for error messages.
+ * @to_name: Destination file name for error messages.
+ *
+ * Return: None, exits on error (98 for read, 99 for write).
+ */
+void copy_file_content(int fd_from, const char *from_name, const char *to_name)
 {
 	char buf[BUF_SIZE];
 	ssize_t r, w, off;
 	int fd_to;
 
-	/* Probe read first so a read error exits 98 before touching destination */
 	r = read(fd_from, buf, BUF_SIZE);
 	if (r == -1)
 	{
@@ -53,7 +79,7 @@ static void copy_stream(int fd_from, const char *from_name, const char *to_name)
 		exit(98);
 	}
 
-	fd_to = xopen_write_trunc(to_name);
+	fd_to = open_file_for_write(to_name);
 
 	while (r > 0)
 	{
@@ -63,7 +89,7 @@ static void copy_stream(int fd_from, const char *from_name, const char *to_name)
 			if (w == -1)
 			{
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_name);
-				xclose(fd_to);
+				close_file(fd_to);
 				exit(99);
 			}
 		}
@@ -71,13 +97,20 @@ static void copy_stream(int fd_from, const char *from_name, const char *to_name)
 		if (r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name);
-			xclose(fd_to);
+			close_file(fd_to);
 			exit(98);
 		}
 	}
-	xclose(fd_to);
+	close_file(fd_to);
 }
 
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: 0 on success, exits with an error code on failure.
+ */
 int main(int argc, char *argv[])
 {
 	int fd_from;
@@ -88,8 +121,8 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = xopen_read(argv[1]);
-	copy_stream(fd_from, argv[1], argv[2]);
-	xclose(fd_from);
+	fd_from = open_file_for_read(argv[1]);
+	copy_file_content(fd_from, argv[1], argv[2]);
+	close_file(fd_from);
 	return (0);
 }
